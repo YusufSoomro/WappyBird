6a2062a6b6b3fd1be324ef71da61e0d28f3ef85d
Yusuf Soomro <yusufsoomro1@gmail.com>
2018-04-07 21:58:11 -0700

    added physics to bird

---
+++ Assets/scripts/TapController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TapController : MonoBehaviour {
+	public float tapForce = 50;
+	public float tiltSmooth = 5;
+	
+	public Vector3 startPos;
+
+	Rigidbody2D rigidBody;
+
+	Quaternion downRotation;
+	Quaternion forwardRotation;
+
+	void Start() {
+		rigidBody = GetComponent<Rigidbody2D>();
+		downRotation = Quaternion.Euler(0, 0, -90);
+		forwardRotation = Quaternion.Euler(0, 0, 35);
+		// rigidBody.simulated = false;
+	}
+
+	void Update() {
+		if(Input.GetMouseButtonDown(0)){
+			transform.rotation = forwardRotation;
+			rigidBody.velocity = Vector3.zero;
+			rigidBody.AddForce(Vector2.up * tapForce, ForceMode2D.Force);
+		}
+		transform.rotation = Quaternion.Lerp (transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if(col.gameObject.tag == "ScoreZone"){
+			// register score event
+			// play a sound
+		}
+
+		if(col.gameObject.tag == "DeadZone"){
+			rigidBody.simulated = false;
+			// register a dead event
+			// play a sound
+			 Debug.Log("hit dead zone");
+		}
+	}
+}
